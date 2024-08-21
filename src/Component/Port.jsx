4cb59c1fde0146c445c8ccb7fb4c44b024b4b9b3
Earthpasserby <iwuno68@gmail.com>
2024-08-21 14:05:20 +0100

    tailwind tutorial

--- src/Component/Port.jsx
+++ src/Component/Port.jsx
@@ -1,10 +1,10 @@
 import React from "react";
-const Port = (year, title, duration, details) => {
+const Port = ({ year, title, duration, details }) => {
   return (
     <>
       <ol className="flex flex-col md:flex-row relative border-l border-stone-200">
         <li>
-          <div></div>
+          <div />
           <p>
             <span>{year}</span>
             <span>{title}</span>
