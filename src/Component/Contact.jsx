30c2e18f03747407925d3d0fdfec91083fe39b66
Earthpasserby <iwuno68@gmail.com>
2024-08-26 13:23:39 +0100

    more on tailwind

--- src/Component/Contact.jsx
+++ src/Component/Contact.jsx
@@ -4,8 +4,8 @@ import { FaLinkedin } from "react-icons/fa6";
 
 const Contact = () => {
   return (
-    <div id="contact">
-      <h1 className="text-4xl font-bold text-center text-green-800">Contact</h1>
+    <div id="contact" className="bg-green-800">
+      <h1 className="text-4xl font-bold text-center text-slate-800">Contact</h1>
       <div className="flex justify-center">
         <div className="flex pt-6  max-w-[200px] w-full justify-between mx-36">
           <FaLinkedin size={30} className="cursor-poointer text-lime-800 " />
@@ -13,11 +13,10 @@ const Contact = () => {
           <FaTwitter size={30} className="cursor-poointer text-lime-800" />
         </div>
       </div>
-      <div>
+      <div className=" ">
         <a>
-          <p className="text-center mt-5 rounded-lg bg-green-800 text-lime-200 text-2xl">
-            GET IN TOUCH
-          </p>
+          <p className=" mt-5 text-lime-200 text-2xl">GET IN TOUCH</p>
+          <p>2024 Iwuno-Samuel. Reserved for gigs</p>
         </a>
       </div>
     </div>
